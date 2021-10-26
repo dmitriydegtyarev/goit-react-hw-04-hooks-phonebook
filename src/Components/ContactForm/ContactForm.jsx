81e79b7640fb2bef834e0fa54c4c8d132a9cc9c4
Dmitriy Dihtiarov <dmitriydegtyarev1985@gmail.com>
2021-10-26 23:16:53 +0300

    replace class to hooks in ContactForm

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,77 +1,75 @@
 // import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 
 import { Form } from './ContactForm.styled';
 
-const INITIAL_STATE = {
-  name: '',
-  number: '',
-};
+export const ContactForm = ({ onAdd, onCheckUnique }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-export class ContactForm extends Component {
-  state = INITIAL_STATE;
-
-  handleChangeForm = ({ target }) => {
+  const handleChangeForm = ({ target }) => {
     const { name, value } = target;
-    this.setState({ [name]: value });
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleFormSubmit = event => {
+  const handleFormSubmit = event => {
     event.preventDefault();
 
-    const { name, number } = this.state;
-    const { onAdd } = this.props;
-
-    const isValidatedForm = this.validateForm();
+    const isValidatedForm = validateForm();
     if (!isValidatedForm) return;
     onAdd({ id: uuid(), name, number });
-    this.resetForm();
+    resetForm();
   };
 
-  validateForm = () => {
-    const { name } = this.state;
-    const { onCheckUnique } = this.props;
-
+  const validateForm = () => {
     return onCheckUnique(name);
   };
 
-  resetForm = () => this.setState(INITIAL_STATE);
+  const resetForm = () => {
+    setName('');
+    setNumber('');
+  };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <Form onSubmit={this.handleFormSubmit}>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChangeForm}
-            placeholder="Enter contact's name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="The name can only consist of letters, apostrophe, dashes and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan etc."
-            required
-          />
-        </label>
-        <label>
-          Number
-          <input
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleChangeForm}
-            placeholder="Enter contact's phone number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-        <button type="submit">Add contact</button>
-      </Form>
-    );
-  }
-}
-
-// ContactForm.propTypes = {};
+  return (
+    <Form onSubmit={handleFormSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChangeForm}
+          placeholder="Enter contact's name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="The name can only consist of letters, apostrophe, dashes and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan etc."
+          required
+        />
+      </label>
+      <label>
+        Number
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChangeForm}
+          placeholder="Enter contact's phone number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <button type="submit">Add contact</button>
+    </Form>
+  );
+};
